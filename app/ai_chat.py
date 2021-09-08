589c9c72aea8ec24e5534dfc7c64b8d1479f0c23
Veera Lupunen <veera.lupunen@helsinki.fi>
2021-09-08 19:59:03 +0300

    Remove unused code

--- app/ai_chat.py
+++ app/ai_chat.py
@@ -5,21 +5,16 @@
 #Laske vastauksen pituus, odota tietty sekuntimäärä ja lähetä viesti oppilaalle
 
 import random
-from .db import db_answer, keywords, fetch_id, fetch_answers
+from .db import keywords, fetch_id, fetch_answers
 
 def ai_answer(message):
     set = keywords()
     words = message.split()
     
     for w in words:
-        w.lower()
+        w = w.casefold()
         if w in set:
-            #hae avainsanan id tietokannasta
             id = fetch_id(w)
-            
-            """hae sanaan liitettyjen vastausten lukumäärä --> voiko yhdistää edelliseen?
-            a_count = count_answers(id)
-            i = random.randint(1,a_count)"""
             
             answers = fetch_answers(id)
             i = random.randint(0, len(answers)-1)
--- app/db.py
+++ app/db.py
@@ -6,20 +6,6 @@ from os import getenv
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
-
-def db_answer(word, i):
-    sql = """SELECT content 
-        FROM aicb.answer a 
-        JOIN aicb.question_answer q_a 
-        ON q_a.a_id = a.id
-        WHERE q_a.q_id=(:word) 
-        AND q_a.a_id=(:i)"""
-        
-    result = db.session.execute(sql, {"word":word, "i":i})
-    answer = result.fetchone()[0]
-    db.session.commit()
-    
-    return answer
     
 def keywords():
     sql = "SELECT word FROM aicb.question"
