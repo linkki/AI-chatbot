32de1d3f5754e855db67d3715300b671acff8f8e
Veera Lupunen <veera.lupunen@helsinki.fi>
2021-09-08 20:10:40 +0300

    Add time delay for aswering

--- app/ai_chat.py
+++ app/ai_chat.py
@@ -4,23 +4,31 @@
 #Lue viesti ja hae tietokannasta satunnainen vastaus
 #Laske vastauksen pituus, odota tietty sekuntimäärä ja lähetä viesti oppilaalle
 
-import random
+import random, time
 from .db import keywords, fetch_id, fetch_answers
 
 def ai_answer(message):
-    set = keywords()
+    if message == "":
+        time.sleep(3)
+        return "Mitä haluaisit kysyä seuraavaksi?"
+        
+    keywords_from_db = keywords()
     words = message.split()
     
     for w in words:
         w = w.casefold()
-        if w in set:
+        if w in keywords_from_db:
             id = fetch_id(w)
             
             answers = fetch_answers(id)
             i = random.randint(0, len(answers)-1)
             
-            return answers[i]
+            answer = answers[i]
+            
+            time.sleep(len(answer)/10 + random.randint(0,3))
+            return answer
         else:
+            time.sleep(1)
             return "Heipä hei! (olen tekoäly)"
 
     
