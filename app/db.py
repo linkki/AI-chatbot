452f53cbe46b25dab91446fce50d9812e5636ac8
Virpi <1564285+viipu@users.noreply.github.com>
2023-03-10 10:47:29 +0200

    applied patch Wrapped-textual-sql-queries-within-text-object-as-sq

--- app/db.py
+++ app/db.py
@@ -3,12 +3,13 @@
 from . import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from sqlalchemy import text
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
     
 def keywords():
-    sql = "SELECT word FROM aicb.question"
+    sql = text("SELECT word FROM aicb.question")
     result = db.session.execute(sql)
     
     keywords = result.fetchall()
@@ -21,7 +22,7 @@ def keywords():
     return keyword_set
     
 def fetch_id(word):
-    sql = "SELECT id FROM aicb.question WHERE word=(:word)"
+    sql = text("SELECT id FROM aicb.question WHERE word=(:word)")
     result = db.session.execute(sql, {"word":word})
     id = result.fetchone()[0]
     db.session.commit()
@@ -29,11 +30,11 @@ def fetch_id(word):
     return id
     
 def fetch_answers(id):
-    sql = """SELECT content 
+    sql = text("""SELECT content 
         FROM aicb.answer a
         JOIN aicb.question_answer q_a 
         ON q_a.a_id = a.id
-        WHERE q_id=(:id)"""
+        WHERE q_id=(:id)""")
     
     result = db.session.execute(sql, {"id":id})
     answers = result.fetchall()
@@ -48,9 +49,9 @@ def fetch_answers(id):
     
 def check_code(code):
     "Returns 0 if code does not exist, 1 if code is a human code and 2 otherwise."
-    sql = """SELECT human
+    sql = text("""SELECT human
         FROM aicb.code c
-        WHERE c.id=(:code)"""
+        WHERE c.id=(:code)""")
         
     result = db.session.execute(sql, {"code":code})
     answer = result.fetchone()
